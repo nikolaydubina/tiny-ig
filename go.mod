0cbedeb8ee1f83a23197e1bc4ee798a255d9909f
Nikolay <nikolay.dubina.pub@gmail.com>
2021-07-25 16:14:09 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nikolaydubina/tiny-ig
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+
+	_ "embed"
+)
+
+//go:embed gallery.html
+var galleryHTMLTemplate string
+
+type PhotoParams struct {
+	Path string
+}
+
+type GalleryParams struct {
+	Photos []PhotoParams
+}
+
+func main() {
+	var (
+		photosDir string
+	)
+
+	flag.StringVar(&photosDir, "dir", "", "path to directory with photos")
+	flag.Parse()
+
+	if photosDir == "" {
+		log.Fatal("dir arg is missing")
+	}
+
+	params := GalleryParams{
+		Photos: []PhotoParams{},
+	}
+
+	files, err := os.ReadDir(photosDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		params.Photos = append(params.Photos, PhotoParams{
+			Path: strings.Join([]string{"", photosDir, file.Name()}, "/"),
+		})
+	}
+
+	parsedTemplate, err := template.New("gallery").Parse(galleryHTMLTemplate)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := parsedTemplate.Execute(os.Stdout, params); err != nil {
+		log.Fatal(err)
+	}
+}
