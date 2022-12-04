d26bbd3197004845463064616a43d4f72010819f
Nikolay <nikolay.dubina.pub@gmail.com>
2022-12-04 21:21:01 +0800

    fix-video

--- ig/video.go
+++ ig/video.go
@@ -22,8 +22,6 @@ func (p Video) Render() string {
 
 func (p Video) RenderInto(out io.StringWriter) {
 	out.WriteString(`<div style="display: contents;">`)
-	out.WriteString(fmt.Sprintf(`<a href="%s" target="_blank" style="display: contents;">`, p.Path))
-	out.WriteString("\n")
 	out.WriteString(fmt.Sprintf(`<video width="%d" height="%d" controls style="margin: 8px; object-fit: cover; width: %dpx; height: %dpx;" loading="lazy">`, p.Width, p.Height, p.Width, p.Height))
 	out.WriteString("\n")
 	out.WriteString(fmt.Sprintf(`<source src="%s" type="video/mp4">`, p.Path))
